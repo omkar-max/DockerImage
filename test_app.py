4ea7a1c1ded288afa37675fad39e77c9b6191815
omkarpasam11@gmail.com <omkarpasam11@gmail.com>
2025-04-15 16:12:50 -0400

    error test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 from app import app
 #  Here pytest will take this file for testing as it starts with test_.
-def test_hello_world(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+def test_home():
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== b"Hello World!"
